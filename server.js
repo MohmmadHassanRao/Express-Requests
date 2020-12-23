c1cf5b1b97d3deed939b2d9642447209796e804e
MohammadHassanRao <hassanribery0335@gmail.com>
2020-12-23 14:45:33 +0500

    interaction with requests

---
+++ server.js
@@ -0,0 +1,23 @@
+// // var express = require("express");
+
+// // var server = express();
+
+// const Http = new XMLHttpRequest();
+// const url = "192.168.50.177:3000/bulb";
+// Http.open("GET", url);
+// Http.send();
+
+// Http.onreadystatechange = (e) => {
+//   console.log(Http.responseText);
+// };
+
+// // server.use(function(req,res,next){
+// //     console.log('URL is: ' ,req.url);
+// //     console.log('Method is: ' ,req.method);
+// //     res.send('some response')
+// //     next()
+// // })
+
+// // server.get('/bulb',function(req,res,next){
+// //     res.send()
+// // })
